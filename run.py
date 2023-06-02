a226a21eeb84dde3f5a85d635469c17280b9aa00
Marley <marley.arns@yahoo.de>
2023-06-02 16:01:05 +0200

    Removed tensorflow requirements

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ import torch
 from pathlib import Path
 import tkinter as tk
 from tkinter import filedialog
-from opennsfw2 import predict_video_frames, predict_image
 from tkinter.filedialog import asksaveasfilename
 import webbrowser
 import psutil
@@ -191,14 +190,9 @@ def start():
         print("\n[WARNING] No face detected in source image. Please try with another one.\n")
         return
     if is_img(target_path):
-        if predict_image(target_path) > 0.85:
-            quit()
         process_img(args['source_img'], target_path, args['output_file'])
         status("swap successful!")
         return
-    seconds, probabilities = predict_video_frames(video_path=args['target_path'], frame_interval=100)
-    if any(probability > 0.85 for probability in probabilities):
-        quit()
     video_name_full = target_path.split("/")[-1]
     video_name = os.path.splitext(video_name_full)[0]
     output_dir = os.path.dirname(target_path) + "/" + video_name if os.path.dirname(target_path) else video_name
