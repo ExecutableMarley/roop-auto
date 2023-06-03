4f8c913afdee775c2156de5adc827b5047761695
Marley <marley.arns@yahoo.de>
2023-06-04 01:45:06 +0200

    Implemented @RichardErkhov multi threaded gpu code

--- core/swapper.py
+++ core/swapper.py
@@ -28,7 +28,7 @@ def swap_face_in_frame(source_face: Face, target_face: Face, frame:Mat):
     return frame
 
 
-def process_faces(source_face: Face, frame: Mat, progress, all_faces=False):
+def process_faces(source_face: Face, frame: Mat, all_faces=False):
     if all_faces:
         many_faces = get_face_many(frame)
         if many_faces:
@@ -48,7 +48,7 @@ def processFramesMany(sourceFace: Face, frame_paths: list):
             frame: Mat = cv2.imread(frame_path)
             try:
                 with HiddenPrints():
-                    result = process_faces(sourceFace, frame, progress, core.globals.all_faces)
+                    result = process_faces(sourceFace, frame, core.globals.all_faces)
                 cv2.imwrite(frame_path, result)
             except Exception:
                 progress.set_postfix(status='E', refresh=True)
@@ -60,7 +60,55 @@ def processFrame(sourceFace: Face, frame_path: str, output_file: str):
     progress_bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
     with tqdm(total=1, desc="Processing", unit="frame", dynamic_ncols=True, bar_format=progress_bar_format) as progress:
         frame: Mat = cv2.imread(frame_path)
-        result = process_faces(sourceFace, frame, progress, core.globals.all_faces)
+        with HiddenPrints():
+            result = process_faces(sourceFace, frame, core.globals.all_faces)
         cv2.imwrite(output_file, result)
-        print("\n\nImage saved as:", output_file, "\n\n")
         progress.update(1)
+    print("\n\nImage saved as:", output_file, "\n\n")
+
+
+from threading import Thread
+
+#creates a thread and returns value when joined
+class ThreadWithReturnValue(Thread):
+    
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+    
+def process_video_gpu(sourceFace: Face, source_video:str, outputfile:str, fps, gpu_threads, all_faces: bool=False):
+    #opening input video for read
+    cap = cv2.VideoCapture(source_video)
+    #opening output video for writing
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    output_video = cv2.VideoWriter( os.path.join(outputfile, "output.mp4"), fourcc, fps, (width, height))
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    temp = []
+    bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
+    with tqdm(total=frame_count, desc='Processing', unit="frame", dynamic_ncols=True, bar_format=bar_format) as progress:
+        while True:
+            #getting frame
+            ret, frame = cap.read()
+            if not ret:
+                break
+            #we are having an array of length %gpu_threads%, running in parallel
+            #so if array is equal or longer than gpu threads, waiting 
+            while len(temp) >= gpu_threads:
+                #we are order dependent, so we are forced to wait for first element to finish. When finished removing thread from the list
+                swappedFrame = temp.pop(0).join()
+                output_video.write(swappedFrame)
+                progress.update(1)
+            #adding new frame to the list and starting it 
+            temp.append(ThreadWithReturnValue(target=process_faces, args=(sourceFace,frame, all_faces)))
+            temp[-1].start()
+        progress.set_postfix(desc="Done", refresh=True)
