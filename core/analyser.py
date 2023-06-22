aa35adcf22c6036d7d56e721636f157f1d82da8e
Marley <marley.arns@yahoo.de>
2023-06-22 23:11:32 +0200

    Updated MultiThreading Code

--- core/analyser.py
+++ core/analyser.py
@@ -1,6 +1,8 @@
 import insightface
+from insightface.app.common import Face
 import core.globals
 from core.hidePrint import HiddenPrints
+from cv2 import Mat
 
 FACE_ANALYSER = None
 
@@ -12,7 +14,7 @@ def get_face_analyser():
     return FACE_ANALYSER
 
 
-def get_face_single(img_data) -> insightface.app.common.Face or None:
+def get_face_single(img_data: Mat) -> insightface.app.common.Face or None:
     with HiddenPrints():
         face = get_face_analyser().get(img_data)
         try:
@@ -21,9 +23,28 @@ def get_face_single(img_data) -> insightface.app.common.Face or None:
             return None
 
 
-def get_face_many(img_data) -> list or None:
+def get_face_many(img_data: Mat) -> list or None:
     with HiddenPrints():
         try:
             return get_face_analyser().get(img_data)
         except IndexError:
             return None
+
+def drawOnFaces(img_data: Mat, faces: list[insightface.app.common.Face]) -> Mat:
+    with HiddenPrints():
+        return get_face_analyser().draw_on(img_data, faces)
+
+def drawOnFace(img_data: Mat, face: insightface.app.common.Face) -> Mat:
+    with HiddenPrints():
+        return get_face_analyser().draw_on(img_data, [face])
+
+def compareFaces(face1: Face, face2: Face, featureCount: int = 10) -> float:
+    if featureCount < 1:
+        featureCount = 1
+    elif featureCount > 100:
+        featureCount = 100 
+    #Compare faces using embeddings
+    sumDif: float = 0.0
+    for i in featureCount:
+        sumDif += abs(face1.embedding_norm[i] - face2.embedding_norm[i])
+    return sumDif / featureCount
--- core/upscale.py
+++ core/upscale.py
@@ -2,10 +2,12 @@ import shutil
 from tqdm import tqdm
 from codeformer.app import inference_app
 from core.hidePrint import HiddenPrints
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, List
 
 #Uses codeformer-pip https://github.com/kadirnar/codeformer-pip
 
-def upscaleImage(sourcePath, outputPath, upscale_factor = 2, fidelity = 0.5):
+def upscaleImage(sourcePath, outputPath, upscale_factor = 1, fidelity = 0.5):
     result = inference_app(
         image=sourcePath, 
         upscale=upscale_factor, 
@@ -15,7 +17,7 @@ def upscaleImage(sourcePath, outputPath, upscale_factor = 2, fidelity = 0.5):
         )
     shutil.move(result, outputPath)
 
-def upscaleReplaceImages(sourcePaths, upscale_factor = 2, fidelity = 0.5):
+def upscaleReplaceImages(sourcePaths, upscale_factor = 1, fidelity = 0.5):
     progress_bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
     with tqdm(total=len(sourcePaths), desc="Processing", unit="frame", dynamic_ncols=True, bar_format=progress_bar_format) as progress:
         for sourcePath in sourcePaths:
@@ -29,3 +31,16 @@ def upscaleReplaceImages(sourcePaths, upscale_factor = 2, fidelity = 0.5):
                     )
             shutil.move(result, sourcePath)
             progress.update(1)
+
+def multiUpscaleImages(sourceDir, upscale_factor = 1, fidelity = 0.5, threadCount: int = 1, progress: Any = None):
+    with ThreadPoolExecutor(max_workers=threadCount) as executor:
+        futures = []
+        for path in sourceDir.iterdir():
+            if path.is_file():
+                future = executor.submit(upscaleImage, path, path, upscale_factor, fidelity)
+                futures.append(future)
+        executor.shutdown(wait=True)
+        for future in futures:
+            future.result()
+            if progress:
+                progress.update(1)
